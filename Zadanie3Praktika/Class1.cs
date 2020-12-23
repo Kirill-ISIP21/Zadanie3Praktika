dc6f33d0b23bea250b525d5bf82654ac32fb9586
k.chukharev@mail.ru <k.chukharev@mail.ru>
2020-12-24 02:08:30 +0300

    Добавьте файлы проекта.

---
+++ Zadanie3Praktika/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3Praktika
+{
+    class Class1
+    {
+        public double ch1;
+        public double ch2;
+    }
+}
---
+++ Zadanie3Praktika/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie3Praktika
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bSum_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 p = new Class1();
+            p.ch1 = Convert.ToDouble(txtB1.Text);
+            p.ch2 = Convert.ToDouble(txtB2.Text);
+            lAnsw.Content = Convert.ToString(p.ch1 + p.ch2);
+        }
+
+        private void bNaib_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 p = new Class1();
+            p.ch1 = Convert.ToDouble(txtB1.Text);
+            p.ch2 = Convert.ToDouble(txtB2.Text);
+            if (p.ch1 > p.ch2)
+            {
+                lNaib.Content = Convert.ToString(p.ch1);
+            }
+            else
+            {
+                lNaib.Content = Convert.ToString(p.ch2);
+            }
+        }
+
+        private void bClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtB1.Text = "";
+            txtB2.Text = "";
+            lNaib.Content = "";
+            lAnsw.Content = "";
+        }
+    }
+}
